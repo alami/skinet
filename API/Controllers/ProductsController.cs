83b1f3750cb4479886606dc9f24bb9cbd6725e93
aminov <alami@tshtt.uz>
2022-03-31 15:44:13 +0500

    ch06-004 Ordering by Name, Price(with convert Decimal 2 Double)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -30,8 +30,8 @@ namespace API.Controllers
 
         }
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(){
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort){
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
             var products = await _productsRepo.ListAsync(spec);
             return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
