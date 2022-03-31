42bb410080e6ccd9ddb97422dc870cd5abcd2055
aminov <alami@tshtt.uz>
2022-03-31 10:37:02 +0500

    ch05-fin Swagger Docs move to Extentions Service&App

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using API.Errors;
+using API.Extentions;
 using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -20,26 +21,12 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
             services.AddAutoMapper(typeof(MappingProfiles) );
             services.AddControllers();
             services.AddDbContext<StoreContext>(x=>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.Configure<ApiBehaviorOptions>(options => {
-                options.InvalidModelStateResponseFactory = ActionContext => {
-                    var errors = ActionContext.ModelState
-                    .Where(e=>e.Value.Errors.Count > 0)
-                    .SelectMany(x=>x.Value.Errors)
-                    .Select(x=>x.ErrorMessage).ToArray();
-                    var errorResponse = new ApiValidationErrorResponse {Errors = errors}; 
-                    return new BadRequestObjectResult(errorResponse);
-                }; 
-            });
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
+            services.AddApplicationServices();            
+            services.AddSwaggerDocumentation();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,8 +35,6 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
@@ -60,6 +45,7 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+            app.UseSwaggerDocumention();
 
             app.UseEndpoints(endpoints =>
             {
